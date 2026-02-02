9210a1d8b0bb6cf133a75cff0133c1d787cd9a19
Ivan Zaburdaev <ivanlom227@gmail.com>
2026-02-02 17:12:29 +0300

    production

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["images.unsplash.com", "zaburdaev.space"],
+    domains: ["images.unsplash.com", "zaburdaev.space", "api.osa-market.ru"],
   },
 };
 
